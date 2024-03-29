1e301847560eb50c4ca6a62de4d965dda76160ba
Anastasiya Pozemina <a.pozyomina@voodoo.rocks>
2024-03-30 01:52:25 +0300

    Fix deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["quasar"],
-  publicPath: "/geometria/",
+  publicPath: process.env.NODE_ENV === "production" ? "/geometria/" : "/",
   pluginOptions: {
     quasar: {
       importStrategy: "kebab",
